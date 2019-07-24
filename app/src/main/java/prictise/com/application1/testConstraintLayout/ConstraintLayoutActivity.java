dd00dd3a4019a048860fd283b917def3655ec711
zhi <1114082241@qq.com>
2019-07-24 13:47:53 +0800

    1.测试ConstraintLayout

--- app/src/main/java/prictise/com/application1/testConstraintLayout/ConstraintLayoutActivity.java
+++ app/src/main/java/prictise/com/application1/testConstraintLayout/ConstraintLayoutActivity.java
@@ -1,7 +1,15 @@
 package prictise.com.application1.testConstraintLayout;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import prictise.com.application1.BaseActivity;
+import prictise.com.application1.R;
 
 public class ConstraintLayoutActivity extends BaseActivity {
 
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_test_constraintlayout);
+  }
 }
